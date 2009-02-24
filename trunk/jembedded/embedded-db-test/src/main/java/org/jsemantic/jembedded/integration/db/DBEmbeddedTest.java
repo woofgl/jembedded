ee6052318c808842436f122a70efb28603930cf9
aestevezjimenez <aestevezjimenez@3d16a578-e97a-11dd-a971-7dbc132099af>
2009-02-24 04:54:32 +0000

    first import db test module that includes db-service.

---
+++ trunk/jembedded/embedded-db-test/src/main/java/org/jsemantic/jembedded/integration/db/DBEmbeddedTest.java
@@ -0,0 +1,35 @@
+package org.jsemantic.jembedded.integration.db;
+
+import java.sql.ResultSet;
+
+
+public interface DBEmbeddedTest {
+	/**
+	 * 
+	 * @param query
+	 * @return
+	 */
+	public ResultSet executeQuery(String query);
+	/**
+	 * 
+	 * @param query
+	 * @return
+	 */
+	public int executeUpdate(String query);
+	/**
+	 * 
+	 * @param query
+	 */
+	public void assertResulSetNotNull(String query);
+	/**
+	 * 
+	 * @param query
+	 */
+	public void assertResulSetNull(String query);
+	/**
+	 * 
+	 * @param query
+	 * @param result
+	 */
+	public void assertEqualsUpdateResult(String query, int result);
+}
---
+++ trunk/jembedded/embedded-db-test/src/main/java/org/jsemantic/jembedded/integration/db/skeletal/AbstractDBEmbeddedTest.java
@@ -0,0 +1,53 @@
+package org.jsemantic.jembedded.integration.db.skeletal;
+
+import java.sql.ResultSet;
+
+import org.jsemantic.jembedded.core.skeletal.AbstractEmbeddTest;
+import org.jsemantic.jembedded.integration.db.DBEmbeddedTest;
+import org.jsemantic.services.dbservice.DBServer;
+
+public abstract class AbstractDBEmbeddedTest extends AbstractEmbeddTest implements DBEmbeddedTest{ 
+	
+	protected void setUp() throws Exception {
+		init();
+	}
+
+	protected void tearDown() throws Exception {
+		release();
+	}
+
+	protected void init() throws Exception {
+		super.init();
+	}
+
+	protected void release() throws Exception {
+		super.release();
+	}
+	
+	protected DBServer getDBService() {
+		return ((DBServer) getService("dbService"));
+	}
+
+	public ResultSet executeQuery(String query) {
+		return getDBService().executeQuery(query);
+	}
+
+	public int executeUpdate(String query) {
+		return getDBService().executeUpdate(query);
+	}
+
+	public void assertResulSetNotNull(String query) {
+		super.assertNotNull(executeQuery(query));
+	}
+
+	public void assertResulSetNull(String query) {
+		super.assertNull(executeQuery(query));
+	}
+
+	public void assertEqualsUpdateResult(String query, int result) {
+		super.assertEquals(result, executeUpdate(query));
+	}
+	
+	public abstract void test() throws Exception;
+	
+}
