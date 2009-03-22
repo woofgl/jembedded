ee517bca185c7af95795b266245186ed354a0684
aestevezjimenez <aestevezjimenez@3d16a578-e97a-11dd-a971-7dbc132099af>
2009-03-22 16:39:31 +0000

    replaced by jintegration-test

--- trunk/jembedded/embedded-webservice/src/main/java/org/jsemantic/jembedded/integration/webservice/WebServiceEmbeddTest.java
+++
@@ -1,7 +0,0 @@
-package org.jsemantic.jembedded.integration.webservice;
-
-import org.jsemantic.jembedded.integration.http.HttpEmbeddTest;
-
-public interface WebServiceEmbeddTest extends HttpEmbeddTest {
-
-}
--- trunk/jembedded/embedded-webservice/src/main/java/org/jsemantic/jembedded/services/webservice/engine/WebServiceEngine.java
+++
@@ -1,7 +0,0 @@
-package org.jsemantic.jembedded.services.webservice.engine;
-
-import org.jsemantic.jembedded.services.httpservice.HttpService;
-
-public interface WebServiceEngine extends HttpService {
-
-}
